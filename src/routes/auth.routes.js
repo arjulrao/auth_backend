e9637ea1d55c3d70694a9855ceeac544c6aeec26
arjulrao <arjulrao21@gmail.com>
2025-08-02 22:53:03 +0530

    Create Register and Login API

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -20,4 +20,36 @@ router.post('/register', async (req, res) => {
     
 })
 
+router.post('/login', async(req, res)=>{
+    const {username, password} = req.body
+
+    const isUserExists = await userModel.findOne({
+        username:username
+    })
+    // console.log(isUserExists)  is give both username and password
+    if(!isUserExists){
+        return res.status(401).json({
+            Message : "User not found.."
+        })
+    } 
+
+    const isPasswordValid = password == isUserExists.password
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            Message : "Please Enter valid Password.."
+        })
+    }
+
+    res.status(200).json({
+        Message : `${username} login success.`
+    })
+
+    // if(isUserExists && isPasswordValid){
+    //     return res.status(200).json({
+    //         Message : `${username} login success.`
+    //     })
+    // }
+})
+
 module.exports = router;
